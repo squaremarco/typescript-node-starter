23457d736bfcbd00bdd395b7c4d35000b9eab193
Marco Cesi <sq@iamsquare.it>
2020-05-06 00:51:39 +0200

    Little config changes

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}', '!src/**/*.d.ts'],
-    roots: ['<rootDir>/src/'],
+    collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}', '!src/**/*.d.ts', 'bin/**/*.{js,jsx,ts,tsx}', '!bin/**/*.d.ts'],
+    roots: ['<rootDir>/src/', '<rootDir>/bin/'],
     testEnvironment: 'node',
     testPathIgnorePatterns: ['/dist/', '/coverage/', '/node_modules/']
 };
